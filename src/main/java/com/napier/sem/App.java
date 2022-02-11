e68af2df7449c0996838d3564f1b1caa42f82360
GuilioM <gigliomarin1995@gmail.com>
2022-02-11 15:30:07 -0600

    Created MongoDB connection and populated with a test

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,7 +1,34 @@
 package com.napier.sem;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+
+import javax.print.Doc;
+
 public class App {
     public static void main(String[] args){
-        System.out.println("Hello World");
+        //Connecting to MongoDB on local system using port 27000
+        MongoClient mongoClient = new MongoClient("localhost", 27000);
+
+        //Get a db
+        MongoDatabase db = mongoClient.getDatabase("mydb");
+
+        //Get a collection from the db
+        MongoCollection<Document> collection = db.getCollection("test");
+
+        //Create Document doc to store
+        Document doc = new Document("name","Guilio M")
+                .append("class","Software Engineering Methods")
+                .append("year", "2022")
+                .append("result", new Document("CW", 95).append("EX", 85));
+
+        //Add doc in collection
+        collection.insertOne(doc);
+
+        //Check doc in collection
+        Document myDoc = collection.find().first();
+        System.out.println(myDoc.toJson());
     }
 }
