da7d513314dbb135b8da2c06514231d5c65d9c62
GuilioM <gigliomarin1995@gmail.com>
2022-03-17 09:00:12 -0600

    Added getEmployee method into App.java

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -69,6 +69,37 @@ public class App
         }
     }
 
+    public Employee getEmployee(int ID) {
+        try {
+            //Create an SQL Statement
+            Statement stmt = con.createStatement();
+            //Create string for SQL statement
+            String strSelect =
+                    """
+                    SELECT emp_no, first_name, last_name
+                    FROM employees 
+                    Where emp_no = 
+                    """ + ID;
+            //Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            //Return new employee if valid
+            // Check one is returned
+            if(rset.next()){
+                Employee emp = new Employee();
+                emp.emp_no = rset.getInt("emp_no");
+                emp.first_name = rset.getString("first_name");
+                emp.last_name = rset.getString("last_name");
+                return emp;
+            }
+            else
+                return null;
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get employee details");
+            return null;
+        }
+    }
 
     public static void main(String[] args)
     {
@@ -77,6 +108,8 @@ public class App
 
         // Connect to database
         a.connect();
+
+        System.out.println(a.getEmployee(10001));
 
         // Disconnect from database
         a.disconnect();
